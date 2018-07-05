ccaae6c044ab8a41a8d7cf0fa3927b402bf82c2a
Adiel <adielp16@gmail.com>
2018-07-05 14:59:16 -0300

    Exercicio 1a

---
+++ Trabalho 2/1/a/pilha.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+struct no{
+	int elem;
+	struct no* prox;
+};
+
+Pilha cria_pilha(){
+	return NULL;
+}	
+int pilha_vazia(Pilha p){
+	return !p;
+}
+int empilhar(Pilha p, int elem){
+	Pilha No = (Pilha) malloc(sizeof(struct no));
+	if(No){
+		No->elem = elem;
+		No->prox = p;
+		p = No;
+		return 1;
+	}
+	return 0;
+}
+int desempilhar(Pilha p, int *elem){
+	if(pilha_vazia(p)) return 0;
+	Pilha No = p;
+	*elem = No->elem;
+	p = No->prox;
+	free(No);
+	return 1;
+}
+int le_topo(Pilha p, int *elem){
+	if(pilha_vazia(p)) return 0;
+	*elem = p->elem;
+	return 1;
+}
+
+void imprimir(Pilha p){
+	if(pilha_vazia(p)) {
+		printf("Pilha = []\n");
+		return;
+	}
+	printf("Pilha = [");
+	Pilha temp = p;
+	while(temp!=NULL){
+		printf("%d ",temp->elem);
+		temp = temp->prox;
+	}
+	printf("\b]\n");
+}
+void imprimir_rev(Pilha p){
+	if(pilha_vazia(p)) {
+		printf("Pilha = []\n");
+		return;
+	}
+	Pilha temp = p;
+	Pilha T = cria_pilha();
+	if(!T) {
+		printf("Ocorreu um erro!\n");
+		return;
+	}
+	while(temp!=NULL){
+		empilhar(T,temp->elem);
+		temp = temp->prox;
+	}
+	imprimir(T);
+}
