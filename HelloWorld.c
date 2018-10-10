0df7b9ef7f60ccc169ecdcd7bf82554921ba4d92
Adiel Pereira <adielp16@gmail.com>
2018-10-10 10:13:57 -0300

    Hello Haskell Java C

---
+++ HelloWorld.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void HelloWorld();
+int main(){
+	HelloWorld();
+	return 0;
+}
+void HelloWorld(){
+	printf("----------------------\n\n");
+	printf("        o        d\n");
+	printf("       l        l\n");
+	printf("        l        r\n");
+	printf("       e        o\n");
+	printf("      h        w\n");
+	printf("\n----------------------\n");
+	printf("    ##############\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##\n");
+	printf("    ##############\n");
+
+}
